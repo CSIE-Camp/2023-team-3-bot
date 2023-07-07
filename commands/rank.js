37e961cba6c05d50a1e7fa7dba8845c37b315bb4
Chuxin <113703030+chuxin3310@users.noreply.github.com>
2023-07-07 09:30:56 +0800

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -17,17 +17,17 @@ module.exports = {
      
         if(bank.length >5 ){
             for (let k = 0; k < 5; k++) {
-            interaction.reply(`第k+1名：${bank[k].Username}的財富為${bank[k].Coin}元`);
+            interaction.reply(`第${k+1}名：${bank[k].Username}的財富為${bank[k].Coin}元`);
         }
         for (let i = 0; i < bank.length; i++) {
             if (bank[i].ID == interaction.user.id){
-                interaction.reply(`第i+1名：${bank[i].Username}的財富為${bank[i].Coin}元`);
+                interaction.reply(`第${i+1}名：${bank[i].Username}的財富為${bank[i].Coin}元`);
            }   
         }
         }
         else{
             for (let k = 0; k < bank.length; k++) {
-                interaction.reply(`第k+1名：${bank[k].Username}的財富為${bank[k].Coin}元`);
+                interaction.reply(`第${k+1}名：${bank[k].Username}的財富為${bank[k].Coin}元`);
             }
         }
         const json = JSON.stringify(bank);
