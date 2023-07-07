badf77b22e79272cc198f22e24ab69602e3ac745
Langmao Tw. 浪貓 <98757802+LangmaoLa@users.noreply.github.com>
2023-07-07 09:22:57 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,6 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+// const { SlashCommandBuilder, EmbedBuilder} = require("discord.js");
+// const { MessageActionRow, MessageButton} = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder().setName("help").setDescription("help"),
     async execute(client, interaction) {
@@ -11,14 +13,22 @@ module.exports = {
             .setImage("https://images.plurk.com/rmIc-1BCPk1VN0bCSqpZCh2HRiZ.gif");
         interaction.reply({ embeds: [help] });
 
-        const link = new ButtonBuilder()
-            .setStyle(ButtonStyle.Link)
-            .setCustomId('link')
-            .setURL('https://github.com/CSIE-Camp/C0in-Mast3r')
-            .setLabel('點我查看所有功能！');
+        // const link = new ButtonBuilder()
+        //     .setStyle(ButtonStyle.Link)
+        //     .setCustomId('link')
+        //     .setURL('https://github.com/CSIE-Camp/C0in-Mast3r')
+        //     .setLabel('點我查看所有功能！');
+
+        const LinkButton = new ButtonBuilder()
+            // .addComponents(
+            // new ButtonBuilder()
+                .setLabel('點我查看所有功能！')
+                .setCustomId('link')
+                .setURL("https://github.com/CSIE-Camp/C0in-Mast3r")
+                .setStyle(ButtonStyle.Link);
         
-        const collectLink = interaction.channel.createMessageComponentCollector({ time: 60000 });
-        console.log(collected);
+        // const collectLink = interaction.channel.createMessageComponentCollector({ time: 60000 });
+        // console.log(collected);
                 
 
     },
